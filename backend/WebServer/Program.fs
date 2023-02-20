5025032cc8a57dbbfc0c7fa3ae6f79d4c1503a06
Ivan Lin <35771002+ivanlinhf@users.noreply.github.com>
2023-02-20 15:29:03 +0000

    Init backend and frontend projects

---
+++ backend/WebServer/Program.fs
@@ -0,0 +1,20 @@
+﻿open Falco
+open Falco.HostBuilder
+open Falco.Routing
+
+let corsHeader = Response.withHeaders ["Access-Control-Allow-Origin", "*"]
+
+type Address = 
+    { City: string 
+      Street: string
+      No: int }
+
+webHost [||] {
+    endpoints [
+        get "/" (corsHeader 
+                 >> Response.ofPlainText "Hello World")
+
+        get "/address" (corsHeader
+                        >> Response.ofJson { City = "SH"; Street = "West NJ"; No = 1129 })
+    ]
+}
