6f6ff4f1ff544abed3ec741e080a3e307f1a1d18
Ivan Lin <35771002+ivanlinhf@users.noreply.github.com>
2023-02-25 11:36:38 +0000

    Use Giraffe and ImageSharp

--- backend/WebServer/Program.fs
+++ backend/WebServer/Program.fs
@@ -1,20 +1,51 @@
-﻿open Falco
-open Falco.HostBuilder
-open Falco.Routing
+﻿open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.PixelFormats
+open System.IO
 
-let corsHeader = Response.withHeaders ["Access-Control-Allow-Origin", "*"]
+let setCorsHeader = setHttpHeader "Access-Control-Allow-Origin" "*"
 
-type Address = 
-    { City: string 
-      Street: string
-      No: int }
+let setImgHeader = setHttpHeader "Content-Type" "image/png"
 
-webHost [||] {
-    endpoints [
-        get "/" (corsHeader 
-                 >> Response.ofPlainText "Hello World")
+let hello () = "Hello World"
 
-        get "/address" (corsHeader
-                        >> Response.ofJson { City = "SH"; Street = "West NJ"; No = 1129 })
-    ]
-}
+let getImage () =
+    use stream = new MemoryStream()
+    use image = new Image<Rgba32>(64, 64)
+    [| 0..63 |] |> Array.iter (fun x -> image[33, x] <- Rgba32(0.1f, 0.5f, 0.3f))
+    image.SaveAsPng stream
+    stream.ToArray()
+
+let webApp =
+    choose
+        [ route "/" >=> setCorsHeader >=> warbler (fun _ -> hello () |> text)
+
+          route "/img"
+          >=> setCorsHeader
+          >=> setImgHeader
+          >=> warbler (fun _ -> getImage () |> setBody) ]
+
+let configureApp (app: IApplicationBuilder) =
+    // Add Giraffe to the ASP.NET Core pipeline
+    app.UseGiraffe webApp
+
+let configureServices (services: IServiceCollection) =
+    // Add Giraffe dependencies
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host
+        .CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(fun webHostBuilder ->
+            webHostBuilder.Configure(configureApp).ConfigureServices(configureServices)
+            |> ignore)
+        .Build()
+        .Run()
+
+    0
